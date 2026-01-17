ed9c1acab652b20d648db9561233b4115040dcc6
milan Offermans <milan.offermans@gmail.com>
2026-01-17 09:39:18 +0100

    init

---
+++ HealthSync/HealthSync/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HealthSync")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HealthSync.Pages")]
