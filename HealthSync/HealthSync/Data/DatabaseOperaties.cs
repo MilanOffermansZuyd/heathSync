a9ed342bc5d909e2217e6f66f1cd986996c0a20a
R92-R92 <2406543kempen@zuyd.nl>
2026-01-21 23:04:20 +0100

    DbOperaties

--- HealthSync/HealthSync/Data/DatabaseOperaties.cs
+++ HealthSync/HealthSync/Data/DatabaseOperaties.cs
@@ -54,6 +54,141 @@ namespace HealthSync.Data
             await Database.SaveChangesAsync();
         }
 
+        // Medication
+        public async Task<List<Medication>> GetMedicationsAsync()
+        {
+            return await Database.Medications.ToListAsync();
+        }
+
+        public async Task<Medication?> GetMedicationByIdAsync(int id)
+        {
+            return await Database.Medications.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task AddMedicationAsync(Medication medication)
+        {
+            Database.Medications.Add(medication);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task UpdateMedicationAsync(Medication medication)
+        {
+            Database.Medications.Update(medication);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task DeleteMedicationAsync(Medication medication)
+        {
+            Database.Medications.Remove(medication);
+            await Database.SaveChangesAsync();
+        }
+
+        // Prescription
+        public async Task<List<Prescription>> GetPrescriptionsAsync()
+        {
+            return await Database.Prescriptions
+                .Include(p => p.Medication)
+                .Include(p => p.Doctor)
+                .Include(p => p.Pharmacy)
+                .ToListAsync();
+        }
+
+        public async Task<List<Prescription>> GetPrescriptionsByUserIdAsync(int userId)
+        {
+            return await Database.Prescriptions
+                .Where(p => p.UserId == userId)
+                .Include(p => p.Medication)
+                .Include(p => p.Doctor)
+                .Include(p => p.Pharmacy)
+                .ToListAsync();
+        }
+
+        public async Task AddPrescriptionAsync(Prescription prescription)
+        {
+            Database.Prescriptions.Add(prescription);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task UpdatePrescriptionAsync(Prescription prescription)
+        {
+            Database.Prescriptions.Update(prescription);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task DeletePrescriptionAsync(Prescription prescription)
+        {
+            Database.Prescriptions.Remove(prescription);
+            await Database.SaveChangesAsync();
+        }
+
+        // HealthData
+        public async Task<List<HealthData>> GetHealthDataAsync()
+        {
+            return await Database.HealthDatas
+                .Include(h => h.User)
+                .ToListAsync();
+        }
+
+        public async Task<HealthData?> GetHealthDataByUserIdAsync(int userId)
+        {
+            return await Database.HealthDatas
+                .Include(h => h.User)
+                .FirstOrDefaultAsync(h => h.UserId == userId);
+        }
+
+        public async Task AddHealthDataAsync(HealthData healthData)
+        {
+            Database.HealthDatas.Add(healthData);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task UpdateHealthDataAsync(HealthData healthData)
+        {
+            Database.HealthDatas.Update(healthData);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task DeleteHealthDataAsync(HealthData healthData)
+        {
+            Database.HealthDatas.Remove(healthData);
+            await Database.SaveChangesAsync();
+        }
+
+        // Notification
+        public async Task<List<Notification>> GetNotificationsAsync()
+        {
+            return await Database.Notifications
+                .Include(n => n.Prescription)
+                .ThenInclude(p => p.Medication)
+                .ToListAsync();
+        }
+
+        public async Task<List<Notification>> GetNotificationsByPrescriptionIdAsync(int prescriptionId)
+        {
+            return await Database.Notifications
+                .Where(n => n.PrescriptionId == prescriptionId)
+                .Include(n => n.Prescription)
+                .ThenInclude(p => p.Medication)
+                .ToListAsync();
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            Database.Notifications.Add(notification);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task UpdateNotificationAsync(Notification notification)
+        {
+            Database.Notifications.Update(notification);
+            await Database.SaveChangesAsync();
+        }
+
+        public async Task DeleteNotificationAsync(Notification notification)
+        {
+            Database.Notifications.Remove(notification);
+            await Database.SaveChangesAsync();
+        }
 
         // Registratie - Email Check
         public async Task<bool> EmailBestaatAsync(string email)
