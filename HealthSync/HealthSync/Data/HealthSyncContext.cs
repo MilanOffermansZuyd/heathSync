fb37f23e8b547780b8317e5e2da56e580a7b9e38
R92-R92 <2406543kempen@zuyd.nl>
2026-01-21 17:51:39 +0100

    asdasd

--- HealthSync/HealthSync/Data/HealthSyncContext.cs
+++ HealthSync/HealthSync/Data/HealthSyncContext.cs
@@ -10,7 +10,7 @@ namespace HealthSync.Data
 
         public HealthSyncContext(DbContextOptions<HealthSyncContext> option) : base(option)
         {
-            Database.EnsureDeleted(); // Tijdelijke fiks voor de database opnieuw te herstellen voor zowel Windows als Android zodat ik niet elke folder af hoef te zoeken
+            Database.EnsureDeleted(); //  TEST Tijdelijke fiks voor de database opnieuw te herstellen voor zowel Windows als Android zodat ik niet elke folder af hoef te zoeken
             Database.EnsureCreated();
         }
 
