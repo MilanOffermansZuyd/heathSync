6828f662639ef339bd1405c5c9542e1b6fba418c
R92-R92 <2406543kempen@zuyd.nl>
2026-01-22 22:09:44 +0100

    Volledig werkend voorbereid op API, nu inclusief mijn afspraken

--- HealthSync/HealthSync/Views/MedicationPage.xaml.cs
+++ HealthSync/HealthSync/Views/MedicationPage.xaml.cs
@@ -10,12 +10,16 @@ public partial class MedicationPage : ContentPage
 	private DatabaseOperaties Database;
     public User IngelogdeUser { get; set; }
     public ObservableCollection<Prescription> Prescriptions { get; } = new();
+    public ObservableCollection<PrescriptionRequest> PrescriptionRequests { get; } = new();
+
+
     public MedicationPage(DatabaseOperaties database, User ingelogdeUser)
 	{
 		InitializeComponent();
 		Database = database;
 		IngelogdeUser = ingelogdeUser;
-		BindingContext = this;	
+		BindingContext = this;
+        SetTab(false);
     }
 
     protected override async void OnAppearing()
@@ -23,11 +27,45 @@ public partial class MedicationPage : ContentPage
         base.OnAppearing();
 
         Prescriptions.Clear();
-        var items = await Database.GetPrescriptionsByUserIdAsync(IngelogdeUser.Id);
+        var prescriptions = await Database.GetPrescriptionsByUserIdAsync(IngelogdeUser.Id);
 
-        foreach (var p in items)
-            Prescriptions.Add(p);
+        foreach (var prescription in prescriptions)
+            Prescriptions.Add(prescription);
+
+        PrescriptionRequests.Clear();
+        var prescriptionRequests = await Database.GetPrescriptionRequestsByUserIdAsync(IngelogdeUser.Id);
+
+        foreach (var request in prescriptionRequests)
+            PrescriptionRequests.Add(request);
     }
+
+    private void SetTab(bool toonAanvragen)
+    {
+        MedicatieView.IsVisible = !toonAanvragen;
+        AanvragenView.IsVisible = toonAanvragen;
+
+        MedicationRequest.IsVisible = !toonAanvragen;
+
+        // Visueel: actief = 1.0, inactief = 0.65
+        BtnTabMedicatie.Opacity = toonAanvragen ? 0.65 : 1.0;
+        BtnTabAanvragen.Opacity = toonAanvragen ? 1.0 : 0.65;
+
+        // Visueel: actief = Bold
+        BtnTabMedicatie.FontAttributes = toonAanvragen ? FontAttributes.None : FontAttributes.Bold;
+        BtnTabAanvragen.FontAttributes = toonAanvragen ? FontAttributes.Bold : FontAttributes.None;
+    }
+
+
+    private void TabMedicatie_Clicked(object sender, EventArgs e)
+    {
+        SetTab(false);
+    }
+
+    private void TabAanvragen_Clicked(object sender, EventArgs e)
+    {
+        SetTab(true);
+    }
+
 
     private async void MedicationRequest_Clicked(object sender, EventArgs e)
     {
@@ -35,7 +73,7 @@ public partial class MedicationPage : ContentPage
         {
             await DisplayAlert("Geen internet", "Je hebt internet nodig om medicatie aan te vragen.", "OK");
             return;
-        }
+        } 
 
         await Navigation.PushAsync(new RequestPrescriptionPage(Database, IngelogdeUser));
     }
