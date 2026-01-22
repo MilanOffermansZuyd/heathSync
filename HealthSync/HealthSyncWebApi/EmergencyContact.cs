385431d02d75b9db77d214c2f4a9ff54c7cc2b3d
milan Offermans <milan.offermans@gmail.com>
2026-01-22 18:22:39 +0100

    api

---
+++ HealthSync/HealthSyncWebApi/EmergencyContact.cs
@@ -0,0 +1,13 @@
+﻿namespace HealthSyncWebApi
+{
+    public class EmergencyContact
+    {
+        public int Id { get; set; }
+
+        public int OnUser { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
