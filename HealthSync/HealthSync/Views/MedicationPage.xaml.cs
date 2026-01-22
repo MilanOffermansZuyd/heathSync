35a5154b540c658b57c07df2caf0b32828ed76d1
R92-R92 <2406543kempen@zuyd.nl>
2026-01-22 01:17:11 +0100

    MedicationPage + Omzetten lifestyle naar border en passend in thema

--- HealthSync/HealthSync/Views/MedicationPage.xaml.cs
+++ HealthSync/HealthSync/Views/MedicationPage.xaml.cs
@@ -1,5 +1,6 @@
 using HealthSync.Data;
 using HealthSync.Models;
+using System.Collections.ObjectModel;
 
 namespace HealthSync.Views;
 
@@ -7,11 +8,23 @@ public partial class MedicationPage : ContentPage
 {
 	private DatabaseOperaties Database;
     public User IngelogdeUser { get; set; }
+    public ObservableCollection<Prescription> Prescriptions { get; } = new();
     public MedicationPage(DatabaseOperaties database, User ingelogdeUser)
 	{
 		InitializeComponent();
 		Database = database;
 		IngelogdeUser = ingelogdeUser;
 		BindingContext = this;	
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Prescriptions.Clear();
+        var items = await Database.GetPrescriptionsByUserIdAsync(IngelogdeUser.Id);
+
+        foreach (var p in items)
+            Prescriptions.Add(p);
     }
 }
